08d21b2de5aeb19f68c53f6d219b7d7ead39374b
Shánenoi <shanenoi.org@gmail.com>
2022-10-20 00:46:15 +0700

    remove unsued file

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-cv2.imshow("original", cv2.cvtColor(image_rgb, cv2.COLOR_RGB2BGR))
-cv2.imshow("gray", image_gray)
-cv2.imshow("noise reduce", cv2.cvtColor(image_less_noise, cv2.COLOR_RGB2BGR))
-cv2.imshow("edge detect", image_detect_edge)
